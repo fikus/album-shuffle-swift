8f50e7accceb27cb9330b075544a1b3f35a3cf8f
Eric Fikus <efikus@gmail.com>
2014-09-09 15:17:21 -0700

    album-shuffle-swift: Initial commit

---
+++ album-shuffle-swift/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  album-shuffle-swift
+//
+//  Created by Eric Fikus on 9/9/14.
+//  Copyright (c) 2014 Eric Fikus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ album-shuffle-swift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  album-shuffle-swift
+//
+//  Created by Eric Fikus on 9/9/14.
+//  Copyright (c) 2014 Eric Fikus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
